4a5f5ca2982badea4f1bc762bdf942d7ad0fa136
camel0t111 <109875158+camel0t111@users.noreply.github.com>
2024-09-19 09:16:54 +0300

    Update RAM.h

--- Laptop/RAM.h
+++ Laptop/RAM.h
@@ -3,7 +3,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 class RAM {
 private:
-    char name[50]; // fixed-size character array for name
+    char name[50];
     double price;
 public:
     RAM(const char* name, double price);
